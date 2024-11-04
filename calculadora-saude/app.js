3ecaf931eb46631a02685e6e4b9bbf78aebade70
Rafael Cardoso <136081646+RafaelCardoso8@users.noreply.github.com>
2024-11-04 19:47:48 -0300

    refactor

--- calculadora-saude/app.js
+++ calculadora-saude/app.js
@@ -5,9 +5,9 @@ const calc_tmb = require('./tmb')
 
 
 const imc = calc_imc(75 , 1.61)
-const percentual_gordura = calc_percentual_gordura("feminino", 75, 1.61, 38)
-const quadril_cintura = calc_quadril_cintura(107,91)
-const tmb = calc_tmb("feminino", 75, 1.61, 38) 
+const percentual_gordura = calc_percentual_gordura("feminino", 75, 1.61, 38 )
+const quadril_cintura = calc_quadril_cintura( 107,91 )
+const tmb = calc_tmb("feminino", 75, 1.61, 38 ) 
 
 console.log(`O IMC é : ${imc}`)
 console.log(`O Percentual de gordura é: é : ${percentual_gordura}`)
